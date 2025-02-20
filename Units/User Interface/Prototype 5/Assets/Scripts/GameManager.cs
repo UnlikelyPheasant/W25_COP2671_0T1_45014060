1b08a0de1ff0c203d39c480da8affda1b5994710
UnlikelyPheasant <ash.burks@go.sfcollege.edu>
2025-02-20 13:09:49 -0500

    Unit 5 lesson 3 completed

--- Units/User Interface/Prototype 5/Assets/Scripts/GameManager.cs
+++ Units/User Interface/Prototype 5/Assets/Scripts/GameManager.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
     private int score;
+
+    public bool isGameActive;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        isGameActive = true;
+
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
     }
@@ -25,7 +33,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
 
@@ -38,6 +46,19 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score: " + score;
+
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 
     }
 }
